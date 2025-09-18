6b6817e93f17e3a4fef93f505971668a3884dbe8
Ravi Kumar <ravibseb96@gmail.com>
2025-09-18 18:17:17 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -33,11 +33,10 @@ app.use("/api/analytics", analyticsRoutes);
 
 //for development
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // ✅ Works in all versions
-  app.use((req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
